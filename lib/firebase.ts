9466ae75fa30c682d8a2ffcbc5c18da8be0c2e34
Chase <chasedemaster.tech@gmail.com>
2024-02-09 16:20:00 -0600

    sdk stuff

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,3 +1,10 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyCa7rOLIgYG7TF0Cd-ThzBuD9gGdu1NoR4",
   authDomain: "post-up-931e2.firebaseapp.com",
@@ -7,3 +14,9 @@ const firebaseConfig = {
   appId: "1:1017931658667:web:3813aed54b01784d224eee",
   measurementId: "G-VTPBRMNDQN"
 };
+
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
+export const auth = getAuth();
+export const storage = getStorage();
