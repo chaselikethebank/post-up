9341700bf3d7c6f461fdcaed450e20533c80e657
Chase <chasedemaster.tech@gmail.com>
2024-02-09 23:21:07 -0600

    svelte is killer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
+<div class="p-4 d-flex float-right bg-center">
+    <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 
-<h1 class="text-3xl text-green font-bold underline">
+<h1 data-theme="cupcake" class="text-3xl">
     Hello world!
   </h1>
+  <button class="btn btn-primary">daisy button here</button></div>
---
+++ src/routes/login/username/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  // import type { PageData } from "./$types";
+
+  // export let data: PageData;
+</script>
+
+
+<h1 class="text-green-500">
+  /login/username
+</h1>
